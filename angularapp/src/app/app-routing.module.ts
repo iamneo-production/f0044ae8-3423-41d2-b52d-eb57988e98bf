87f85707084f8ed673c34948b96b114542cef825
vinothcse107 <83579165+vinothcse107@users.noreply.github.com>
2022-01-28 19:27:23 +0530

    signup page & Routing

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { AddPostpaidComponent } from './admin/postpaid/add-postpaid/add-postpaid.component';
+import { EditPostpaidComponent } from './admin/postpaid/edit-postpaid/edit-postpaid.component';
+import { PostpaidComponent } from './admin/postpaid/postpaid.component';
+import { ViewPostpaidComponent } from './admin/postpaid/view-postpaid/view-postpaid.component';
+import { AddPrepaidComponent } from './admin/prepaid/add-prepaid/add-prepaid.component';
+import { EditPrepaidComponent } from './admin/prepaid/edit-prepaid/edit-prepaid.component';
+import { PrepaidComponent } from './admin/prepaid/prepaid.component';
+import { ViewPrepaidComponent } from './admin/prepaid/view-prepaid/view-prepaid.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+    ],
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      {
+        path: 'postpaid',
+        component: PrepaidComponent,
+        children: [
+          { path: 'view-post-paid', component: ViewPostpaidComponent },
+          { path: 'edit-post-paid/:id', component: EditPostpaidComponent },
+          { path: 'add-post-paid', component: AddPostpaidComponent },
+        ],
+      },
+      {
+        path: 'prepaid',
+        component: PostpaidComponent,
+        children: [
+          { path: 'view-pre-paid', component: ViewPrepaidComponent },
+          { path: 'edit-pre-paid/:id', component: EditPrepaidComponent },
+          { path: 'add-pre-paid', component: AddPrepaidComponent },
+        ],
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularapp/src/app/auth/signup/signup.component.ts
+++ angularapp/src/app/auth/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from 'src/app/services/login.service';
+import { UserModel } from 'src/app/shared/UserModel';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _loginService : LoginService ) { }
 
-  ngOnInit(): void {
-  }
+    // Reactive Forms Initilization & Validation
+    SignupForm!: FormGroup;
+    ngOnInit(): void {
+        this.SignupForm = new FormGroup({
+            'email': new FormControl(null, [Validators.required, Validators.email]),
+            'username' : new FormControl(null, [Validators.required , Validators.pattern("[a-zA-Z0-9_]{8,12}")]),
+            'mobileNumber': new FormControl(null, [Validators.required , Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
+            'password': new FormControl(null, [Validators.required, Validators.pattern("[a-zA-Z0-9_@#$!?></|+*]{8,16}")]),
+            'confirmPass': new FormControl(null, [Validators.required ,Validators.pattern("[a-zA-Z0-9_@#$!?></|+*]{8,16}") ])
+        })
+    }
+
+
+    // Checking Match of Passwords
+    InvalidPass: boolean = false;
+    // Checks On Change If Password and Confirm Password are same
+    PassCheck() {
+        let pass = this.SignupForm.get('password')?.value;
+        let cpass = this.SignupForm.get('confirmPass')?.value;
+        console.log(typeof(pass) +"   "+pass.length)
+        if(pass.length>= 8 && cpass.length >=8){
+            if (pass === cpass) {
+                this.InvalidPass = true;
+            }
+            else {
+                this.InvalidPass = false;
+            }
+        }
+        else{
+            this.InvalidPass = false;
+        }
+    }
+
+    /**
+     * SignUp and Login Button Actions
+     * Get Value from Fields Only if Valid
+     */
+    onSubmit() {
+        const body : UserModel = {
+            email : this.SignupForm.get('email')?.value,
+            password : this.SignupForm.get('password')?.value,
+            username : this.SignupForm.get('username')?.value,
+            mobileNumber : this.SignupForm.get('mobileNumber')?.value,
+            userRole : "user"
+        }
+
+        // Post the Signup Value in User Model
+        this._loginService.SignupCheck(body).subscribe();
+
+        // Navigating Back To Login Page
+        setTimeout(() => {
+            this._loginService.router.navigate(['auth', 'login']);
+        }, 100);
+    }
 
 }
