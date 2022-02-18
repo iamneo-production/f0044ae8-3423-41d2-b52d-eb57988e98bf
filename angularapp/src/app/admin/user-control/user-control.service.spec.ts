c9d9406ca8f358416bfbdd6814d69dadd79cb25b
vennela752001 <vennela752001@gmail.com>
2022-02-18 19:09:29 +0530

    UserComponentChanges

--- angularapp/src/app/admin/user-control/user-control.service.spec.ts
+++ angularapp/src/app/admin/user-control/user-control.service.spec.ts
@@ -1,16 +1,61 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserModel } from 'src/app/shared/UserModel';
 
-import { UserControlService } from './user-control.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserControlService {
 
-describe('UserControlService', () => {
-  let service: UserControlService;
+  constructor(public http: HttpClient, public router: Router) { }
+  BaseUrl = "https://localhost:5001/admin/users/";
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserControlService);
-  });
+    // *----------------*--------------*-------------
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    /**
+     * Get All Users On Startup
+     */
+    Plans!: UserModel[];
+    GetUsers(): Observable<UserModel[]> {
+        let Url = this.BaseUrl+"getUsers";
+        return this.http.get<UserModel[]>(Url)
+    }
+
+    // *----------------*--------------*-------------
+    /**
+     * @param PlanId
+     * Action : Get Addon with specific Id
+     * return AddonsModel
+     * 
+     * Used In AddonComponent Class
+     */
+
+    Plan!: UserModel;
+    GetUser(email : string): Observable<UserModel> {
+        let Url = this.BaseUrl+"getUser/"+email;
+        return this.http.get<UserModel>(Url);
+    }
+
+    // *----------------*--------------*-------------
+
+    //Edit User | Put Request
+
+    EditId!: UserModel;
+    EditUser(params: string, body: UserModel):Observable<any>{
+        let Url = this.BaseUrl+"editUser/"+params;
+        console.log(body);
+        const headers = { 'content-type': 'application/json' };
+        return this.http.put<UserModel>(Url , JSON.stringify(body), { headers: headers });
+    }
+
+    // *----------------*--------------*-------------
+
+    // Delete User Delete Request
+    DeleteUser(Email: string):Observable<any> {
+        let DeleteUrl = this.BaseUrl+"deleteUser/";
+        return this.http.delete<UserModel>(DeleteUrl + Email)
+    }
+
+}
