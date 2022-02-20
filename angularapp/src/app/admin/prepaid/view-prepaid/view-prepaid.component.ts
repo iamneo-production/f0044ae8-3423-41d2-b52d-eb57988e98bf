c25b38338eb1b310a91f174adeb970281cfcfa3d
likhith-9 <likhithkrishna72@gmail.com>
2022-02-20 12:40:35 +0000

    prepaid,navbar,popularplans

--- angularapp/src/app/admin/prepaid/view-prepaid/view-prepaid.component.ts
+++ angularapp/src/app/admin/prepaid/view-prepaid/view-prepaid.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PostpaidService } from '../../postpaid/postpaid.service';
+import { PlanModel } from '../../../shared/PlanModel';
+import { PrepaidService } from '../prepaid.service';
 
 @Component({
   selector: 'app-view-prepaid',
@@ -7,9 +10,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewPrepaidComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _prePaidService: PrepaidService) {
+  }
 
-  ngOnInit(): void {
+  Plans: PlanModel[] = [];
+  public ngOnInit(): void {
+      this.GetPlans();
+      setTimeout(() => {
+          this.ShowPrepaidOnly();
+      }, 100);
+
+  }
+
+  /**
+     * @Action Gets all the Plans
+     * Assign in Plans Variable
+  */
+
+  GetPlans() {
+      this._prePaidService.GetPlans().subscribe(data => {
+          this.Plans = data;
+      });
+  }
+
+  /**
+    * Extract All the  Plans from thr PlanModel
+    * Seperates the Plans Whichs are of Prepaid plans
+    * to Display Only PrePaidPlans
+    */
+  PrePaidPlans: PlanModel[] = [];
+  ShowPrepaidOnly() {
+      let i: number;
+      for (i = 0; i < this.Plans.length; i++) {
+          if (this.Plans[i].planType === "PrePaid") {
+              this.PrePaidPlans.push(this.Plans[i]);
+          }
+      }
+  }
+
+
+  /**
+   * Get the Addon Of Specific Item
+   * @param index
+   */
+  GetAddon(index: number) {
+      this._prePaidService.GetPlan(this.Plans[index].planId).subscribe();
+  }
+
+
+  /**
+   * Assigning the Value in Service
+   * to Receive from EditAddonsComponent
+   * @param index
+   */
+  EditSelectPlan(index: number) {
+      this._prePaidService.EditId = this.Plans[index];
+  }
+
+  /**
+   * Delete the Specific Item from view and DB
+   * @param index
+   */
+  OnDelete(index: number) {
+      this._prePaidService.OnDelete(this.Plans[index].planId).subscribe();
+      this.PrePaidPlans.splice(index, 1);
+  }
+
+
+  /**
+   * Navigating to Add Addon
+  */
+  AddPlan() {
+      this._prePaidService.router.navigate(['/admin/prepaid/add-pre-paid']);
   }
 
 }
--- angularapp/src/app/client-view/popularplans/popularplans.component.ts
+++ angularapp/src/app/client-view/popularplans/popularplans.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AddonsModel } from 'src/app/shared/AddonModel';
+import { PlanModel } from 'src/app/shared/PlanModel';
+import { RechargeModel } from 'src/app/shared/RechargeModel';
+import { UserService } from '../user.service';
+
 
 @Component({
   selector: 'app-popularplans',
@@ -7,9 +13,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopularplansComponent implements OnInit {
 
-  constructor() { }
+  User!: string;
+    RechargeForm!: FormGroup;
+    SetPlanRecharge!: PlanModel;
+    user!: string;
 
-  ngOnInit(): void {
-  }
+    constructor(private _userService: UserService) { 
+        this.user = this._userService.CurrentUser;
+    }
+
+    Plans!: PlanModel[];
+    public ngOnInit(): void {
+        this.GetPlans();
+
+        this.RechargeForm = new FormGroup({
+            _RechargeType: new FormControl("PrePaid", [Validators.required]),
+            _Mobile: new FormControl(null, [Validators.required , Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
+            _Email: new FormControl(null, [Validators.required]),
+        });
+    }
+
+    GetPlans() {
+        this._userService.GetPlans().subscribe((data) => {
+            this.Plans = data;
+        });
+    }
+
+    ToRecharge(index: number) {
+        this.SetPlanRecharge = this.Plans[index];
+        this.ngOnInit();
+    }
+
+
+    generateRandomNumber(): number {
+        var minm = 100000;
+        var maxm = 999999;
+        return Math.floor(Math.random() * (maxm - minm + 1)) + minm;
+    }
+    PutRechargeDetails() {
+        // gets The value from the Fields
+        const body: RechargeModel = {
+            rechargeId: this.generateRandomNumber(),
+            rechargetype: this.RechargeForm?.get('_RechargeType')?.value,
+            name: this.user,
+            mobile: this.RechargeForm.get('_Mobile')?.value,
+            email: this.RechargeForm.get('_Email')?.value,
+            rechargePlan: this.SetPlanRecharge?.planName,
+            rechargePrice: this.SetPlanRecharge?.planPrice,
+            planDetails : ""
+        };
+
+        // Post Request from Admin Service.
+        this._userService.PutRecharge(body).subscribe((data) => {
+            // this._userService.router.navigate([this.user,'viewplans']);
+            console.log(data);
+        });
+      }
 
 }
